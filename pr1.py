91316e0c911ccb1ebe018a1e650b43f209c8fd70
Adesh <adesh717262+git@gmail.com>
2024-09-01 17:57:37 +0530

    updated guess game

--- pr1.py
+++ pr1.py
@@ -5,16 +5,18 @@ print(f"guess the number between 1 and 10")
 def guess_number(r):
     count = 0
     while True:
-        num = int(input("Enter a number to guess: "))
+        num = int(input(f"Enter a number to guess: "))
         count += 1
         if num == r:
             print(f"You guessed correctly! \n\tIn {count} guesses")
             break
-        elif num > r:
-            print(f"please enter a number less than {num}")
         elif num < r:
             print(f"please enter a number greater than {num}")
         else:
-            print(f"entered no is out of range")
+            if (num >= 1 and num <= 10):
+                print(f"please enter a number less than {num}")
+            else:
+                print(f"entered no is out of range")
+            
 r = rd.randint(1,10)
 guess_number(r)
